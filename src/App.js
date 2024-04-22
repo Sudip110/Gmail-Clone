667f71c24ef1157859df356c37769ac9375364a6
sudip110 <sudiptom172@gmail.com>
2024-04-22 09:12:49 +0530

    add user persistance on login functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Mail from "./components/Mail";
@@ -7,14 +7,32 @@ import Sidebar from "./components/Sidebar";
 import EmailList from "./components/EmailList";
 import Compose from "./components/Compose";
 import { selectSendMessageIsOpen } from "./features/mailSlice.js";
-import { useSelector, Provider } from "react-redux";
+import { useSelector, Provider, useDispatch } from "react-redux";
 import { store } from "./app/store.js";
 import Login from "./components/Login.js";
-import { selectUser } from "./features/userSlice.js";
+import { login, selectUser } from "./features/userSlice.js";
+import { auth } from "./Firebase/firebase.js";
 function App() {
   const sendMailIsOpen = useSelector(selectSendMessageIsOpen);
   const user =useSelector(selectUser);
+  const dispatch = useDispatch();
   console.log(sendMailIsOpen);
+
+  useEffect(()=>
+  {
+    auth.onAuthStateChanged((user)=>
+    {
+      if(user)
+      {
+        dispatch(login(
+          {
+                displayName:user.displayName,
+                userEmail:user.email,
+                userPhotoUrl:user.photoURL,
+          }))
+      }
+    })
+  },[])
   return (
     <Router>
       {!user?
