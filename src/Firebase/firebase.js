6a1930cc3afd9afc76677cc176ec92c3202a9a20
sudip110 <sudiptom172@gmail.com>
2024-04-23 12:26:53 +0530

    add env variables to config files

--- src/Firebase/firebase.js
+++ src/Firebase/firebase.js
@@ -7,12 +7,12 @@ import { GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCKPIbVp6rZIOxoHmFAmmliumqOIr6uIY8",
-  authDomain: "clone-8b9c5.firebaseapp.com",
-  projectId: "clone-8b9c5",
-  storageBucket: "clone-8b9c5.appspot.com",
-  messagingSenderId: "135623121962",
-  appId: "1:135623121962:web:daa2c167658d9ff0d73753",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APPLICATION_ID,
 };
 
 // Initialize Firebase
